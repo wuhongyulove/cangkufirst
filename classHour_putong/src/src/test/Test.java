7732f92337384fb70d017f3c6fe3d3be2408321d
wuhongyulove <wuhongyu_company@163.com>
2022-12-27 09:13:05 +0800

    第二次提交 添加 我爱你

--- classHour_putong/src/src/test/Test.java
+++ classHour_putong/src/src/test/Test.java
@@ -3,7 +3,7 @@ package src.test;
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("你好欢迎你\t亲爱的小孩");
+		System.out.println("你好欢迎你\t亲爱的小孩\t我爱你");
 		System.out.println("你好欢迎你\t张佳宁");
 		System.out.println("你好欢迎你\t吴宏宇");
 	}
