45c24555db0e66ac6ab98dfdec87630761188601
wuhongyulove <wuhongyu_company@163.com>
2022-12-27 09:30:14 +0800

    第五次提交

--- classHour_putong/src/src/test/Test.java
+++ classHour_putong/src/src/test/Test.java
@@ -5,6 +5,7 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("你好欢迎你\t亲爱的小孩");
 		System.out.println("你好欢迎你\t张佳宁\tfirst");
+		System.out.println("123...");
 		System.out.println("你好欢迎你\t吴宏宇\tSecond");
 	}
 
